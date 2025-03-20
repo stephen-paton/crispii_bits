c434d5e85f57eaf98815ddabfe182bb825ab4e73
Stephen Paton <69050696+stephen-paton@users.noreply.github.com>
2025-03-20 21:39:45 +0000

    SetBit trait implemented for all native rust uint types

---
+++ README.md
@@ -0,0 +1,5 @@
+# crispii_bits
+
+Bit related functionality intended for use with Crispii
+
+License: MIT OR Apache-2.0
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+//! Bit related functionality intended for use with Crispii
+pub mod set_bit;
+pub use set_bit::SetBit;
